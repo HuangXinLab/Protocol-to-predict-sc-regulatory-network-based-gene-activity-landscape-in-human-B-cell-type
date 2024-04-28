85b74e186d11af13ee4b30295f1c37d67ef9469d
asdChenJin <33625968+asdChenJin@users.noreply.github.com>
2024-04-28 15:28:41 +0800

    Create 2.1.generateSJARACNeInput.R

---
+++ 2.1.generateSJARACNeInput.R
@@ -0,0 +1,33 @@
+library(scMINER)
+library(Seurat)
+library(NetBID2)
+library(ggplot2)
+
+# 1_seurat2eset #######
+load('./B_sub.rdata')
+B_sub=RenameIdents(B_sub,"Immature B"="ImmatureB","Mature B"="MatureB")
+levels(B_sub) <- c('HSC',"CLP",'Pre-pro-B','Pro-B','Pre-B','ImmatureB','MatureB','Plasma')
+B_sub$mergeCycling <- Idents(B_sub)
+#expression_data
+expression_data<- B_sub@assays$RNA@data 
+#cell_metadata: 
+data<-B_sub@meta.data
+data$cellName<-data$mergeCycling
+cell_metadata<-new('AnnotatedDataFrame', data = data)
+#gene_annotation: 
+gene_annotation <- data.frame(gene_short_name =as.character (row.names(expression_data)), geneSymbol=as.character (row.names(expression_data)))
+rownames(gene_annotation)<-gene_annotation$gene_short_name
+gene_annotation$geneSymbol<-gene_annotation$gene_short_name
+gene_annotation$nCells<-Matrix::rowSums(expression_data!=0)
+#new eset
+eset <- CreateSparseEset(data=expression_data, meta.data = cell_metadata@data, feature.data = gene_annotation, add.meta = F)
+# filter out genes expressed in less than 50cells
+eset.filter.cell.log2 <- eset[fData(eset)$nCells>50,] 
+
+# 2_generateSJARACNeInput #######
+# ref data is tf_sigs_hg.RData
+generateSJARACNeInput(
+input_eset = eset.filter.cell.log2, funcType = NULL, 
+ref = "hg",  #human
+wd.src = "SJARACNe",  #Output directory
+group_name = "mergeCycling")
