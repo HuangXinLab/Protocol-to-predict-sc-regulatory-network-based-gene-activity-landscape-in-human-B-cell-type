8ba0865b52525ae17ffdf33614e97f020500714e
asdChenJin <33625968+asdChenJin@users.noreply.github.com>
2024-04-28 16:02:40 +0800

    Create 2.2.generateSJARACNenetwork.sh

---
+++ 2.2.generateSJARACNenetwork.sh
@@ -0,0 +1,9 @@
+#!usr/bin/bash 
+
+# python: 3.7.6
+# SJARACNe: https://github.com/jyyulab/SJARACNe
+
+indir = ./SJARACNe
+cd $indir
+for i in $(ls -d */ | cut -f1 -d'/'); do
+echo sjaracne lsf -j $indir/config_cwlexec.json -e $indir/${i}/*.exp -g > $indir/${i}/tf/*.txt -n 100 -o $indir/${i}/tf -pc 0.01;
