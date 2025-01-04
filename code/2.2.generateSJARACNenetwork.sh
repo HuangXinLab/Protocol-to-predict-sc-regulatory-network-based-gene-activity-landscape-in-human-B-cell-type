45f715bf7b29323bf7d1aec71ad6e7b1e802e6f4
hxt163 <115469513+hxt163@users.noreply.github.com>
2025-01-04 16:25:36 +0800

    Delete code/2.2.generateSJARACNenetwork.sh

--- code/2.2.generateSJARACNenetwork.sh
+++
@@ -1,9 +0,0 @@
-#!usr/bin/bash 
-
-# python: 3.7.6
-# SJARACNe: https://github.com/jyyulab/SJARACNe
-
-indir = ./SJARACNe
-cd $indir
-for i in $(ls -d */ | cut -f1 -d'/'); do
-echo sjaracne lsf -j $indir/config_cwlexec.json -e $indir/${i}/*.exp -g > $indir/${i}/tf/*.txt -n 100 -o $indir/${i}/tf -pc 0.01;
